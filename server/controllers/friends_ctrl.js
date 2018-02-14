2675cdb7dd1fa91befc5b7b3575d14f51bb557ce
Ben Johnson <holloo111@gmail.com>
2018-02-13 21:19:16 -0700

    controllers v1 done

--- server/controllers/friends_ctrl.js
+++ server/controllers/friends_ctrl.js
@@ -12,14 +12,31 @@ module.exports = {
             { first_name, last_name } = req.body
             
         db.search_friends([ user_id, first_name, last_name ])
+            .then( friends => res.status(200).send( friends ) )
     },
     createFriendship: function( req, res ) {
-        const db = req.app.get( 'db' )
+        const db = req.app.get( 'db' ),
+            { user_id } = req.user,
+            { friend_id } = req.body
+
+            db.search_friends([ user_id, friend_id ])
+            .then( () => res.status(200).send( "Friendship Pending" ) )
     },
     confirmFriend: function( req, res ) {
-        const db = req.app.get( 'db' )
+        const db = req.app.get( 'db' ),
+            { user_id } = req.user,
+            { friend_id } = req.body
+
+        db.accept_friendship([ user_id, friend_id ])
+            .then( () => res.status(200).send( "Friend Accepted" ) ) 
+
     },
     deleteFriend: function( req, res ) {
-        const db = req.app.get( 'db' )
+        const db = req.app.get( 'db' ),
+            { user_id } = req.user,
+            { friend_id } = req.body
+
+        db.delete_friendship([ user_id, friend_id ])
+            .then( () => res.status(200).send( "Friend Deleted" ) ) 
     }
 }
--- server/controllers/user_ctrl.js
+++ server/controllers/user_ctrl.js
@@ -1,6 +1,10 @@
 module.exports = {
     updateUser: function( req, res ) {
-        const db = req.app.get( 'db' )
-        db.update_user([  ])
+        const db = req.app.get( 'db' ),
+            { user_id } = req.user,
+            { first_name, last_name, location } = req.body
+
+        db.update_user([ user_id, first_name, last_name, location ])
+            .then( ()=> res.status(200).send( 'User Updated' ) )
     }
 }
