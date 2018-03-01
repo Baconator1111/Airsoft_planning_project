6fea13ee83f8982f76fda2132ca5e85e9243fd0b
Ben Johnson <holloo111@gmail.com>
2018-03-01 15:18:41 -0700

    dotenv

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import App from './App';
 import { SocketProvider } from 'socket.io-react';
 import io from 'socket.io-client';
 
-require('dotenv').config()
-
 const socket = io.connect(process.env.REACT_APP_SOCKET_CONNECTION);
 
 ReactDOM.render(
