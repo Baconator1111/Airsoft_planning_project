da3c71273e7905ab2bed92dd870108c8af3b383f
Ben Johnson <holloo111@gmail.com>
2018-03-04 15:25:27 -0700

    allows user_type to be accesed

--- db/get_posts.sql
+++ db/get_posts.sql
@@ -1,4 +1,4 @@
-select user_name, user_img, post_id, user_id_posts, post_title, post_img, post_body
+select user_name, user_type, user_img, post_id, user_id_posts, post_title, post_img, post_body
 from posts
 join users on user_id = user_id_posts
 where post_id not in ( select post_id_filter from filtered_posts where user_id_filter = $1 )
